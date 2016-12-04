ff585c4a27dbab6eb254ca33782db43176dc9135
Ronny Mathew <ronnygeo@Ronnys-MacBook-Pro.local>
2016-12-04 14:55:47 -0500

    Switching to RDD for initial read and splits.

--- src/main/scala/BirdClassifier.scala
+++ src/main/scala/BirdClassifier.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -10,17 +11,32 @@ object BirdClassifier {
       .setAppName("Bird Classifier")
      .setMaster("local")
 
-    val spark = SparkSession
-      .builder()
-      .config(conf)
-      .getOrCreate()
+//    val spark = SparkSession
+//      .builder()
+//      .config(conf)
+//      .getOrCreate()
+
+    val sc = new SparkContext(conf)
+
+    // For implicit conversions like converting RDDs to DataFrames
+//    import spark.implicits._
 
     //Initializing constants
     var time = System.currentTimeMillis()
 
-    var input: String = "in/unlabeled-small.csv"
+    var input: String = "in/labeled-small2.csv"
     var output:String = "output"
+    val escapCols = 19 to 952
+    val duplicateCols = Seq(1016, 1017)
 
+    //1016, 1017 duplicate
+    //19:954
+
+    // String fields: 1, 9:12, 959
+
+    //Class label: 26
+
+    //Drop: 0, 15, 17
 
     //if output is specified, use that else default
     if (args.length > 1) {
@@ -28,27 +44,58 @@ object BirdClassifier {
       output = args(1)
     }
 
-    //Loading the input files and getting the training set
-    val input_df = spark.read
-      .format("csv")
-      .option("header", "true") // Use first line of all files as header
-      .option("inferSchema", "true") // Automatically infer data types
-      .load(input)
+    val inputRDD = sc.textFile(input, 2).map(line => line.split(","))
+
+
+    def splitArr(arr: Array[String], s:Int, offset:Int) : Array[String] = {
+      val (p1, p2) = arr.splitAt(s)
+      p1 ++ (p2.drop(offset))
+    }
+
+    val newRDD = inputRDD.map{arr =>
+      val arr2 = splitArr(arr, 19, 936)
+      splitArr(splitArr(arr2, 80, 2), 0, 1).toString
+    }
+
+    newRDD.saveAsTextFile("newRDD")
 
     //TODO: Drop duplicate columns
-    input_df.drop("SAMPLING_EVENT_ID").select("CAUS_FIRST_AUTUMN_32F_MEAN").write.format("parquet").save(output+"/samplingid.parquet")
+    //Loading the input files and getting the training set
+//    val input_df = spark.read
+//      .format("csv")
+//      .option("header", "true") // Use first line of all files as header
+//      .option("inferSchema", "true") // Automatically infer data types
+//      .load(input)
 
+//    val Y = input_df.select("Agelaius_phoeniceus").map(row => !row(0).equals("0"))
+//    val X = input_df.drop(input_df.columns.zipWithIndex.filter(t => (escapCols.contains(t._2))
+//      // || duplicateCols.contains(t._2))
+//    ).map(t => t._1):_*)
+
+//    X.write.format("csv").save(output+"/samplingid.csv")
+
+//    Y.show(1000)
     time = System.currentTimeMillis()
 
     //Getting the initial N value
-    // var N = adj.count
     println("Preprocessing Time: " + (System.currentTimeMillis() - time)+"ms")
 
-    // wiki_data.unpersist()
-    //Starting the iterator value
-    // var i = 0
+    // Create a LogisticRegression instance. This instance is an Estimator.
+    val lr = new LogisticRegression()
+    // Print out the parameters, documentation, and any default values.
+    println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
 
-    //Stopping the spark context
-    spark.stop()
+    // We may set parameters using setter methods.
+    lr.setMaxIter(10)
+      .setRegParam(0.01)
+
+    // Learn a LogisticRegression model. This uses the parameters stored in lr.
+//    val model1 = lr.fit(X)
+
+
+
+    //Stopping the spark session
+    sc.stop()
+//    spark.stop()
   }
 }
